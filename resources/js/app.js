72123092ac4d83196dce031a7b9a717055a7c47d
Simon Pangan <simonjoseph.pangan@gmail.com>
2021-02-08 09:11:42 +0800

    Global Vuejs Filters and Momentj

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+
+import moment from 'moment';
 import { Form, HasError, AlertError } from 'vform'
 import VueRouter from 'vue-router';
-
 
 //global  // means can access in any url
 window.Form = Form;
@@ -22,6 +23,13 @@ let routes = [
     { path: '/users', component: require('./components/Users.vue').default },
   ]
 
+Vue.filter('upText',(text) => {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+Vue.filter('myDate',(created) => {
+  return moment(created).format('MMMM Do YYYY, h:mm:ss a');
+});
 
 const router = new VueRouter({
   mode:'history', 
