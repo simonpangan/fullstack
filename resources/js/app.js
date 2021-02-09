e14f3ceea8add0861fbdce1e5c01b624df3e14dc
Simon Pangan <simonjoseph.pangan@gmail.com>
2021-02-09 14:53:33 +0800

    Add ACL to Laravel Without Extra Tables in Database

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
     { path: '/profile', component: require('./components/Profile.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
+    { path: '/developer', component: require('./components/Developer.vue').default },
   ];
 
 Vue.filter('upText',(text) => {
---
+++ resources/js/components/Developer.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="container">
+      asad
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  mounted() {
+    console.log("DEVELOPER VUE");
+  },
+};
+</script>
